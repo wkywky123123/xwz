54e205d3b3d931cce932eacd9b0cb10086a340fc
忾氧 <89382167+wkywky123123@users.noreply.github.com>
2025-05-31 10:41:50 +0800

    Delete js/effect.js

--- js/effect.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('img[alt="gift"]').addEventListener('click', () => {
-  const fireworks = document.createElement('div');
-  fireworks.className = 'firework';
-  document.body.appendChild(fireworks);
-  setTimeout(() => fireworks.remove(), 1000);
-});
